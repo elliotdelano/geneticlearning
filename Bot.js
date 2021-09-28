0fcf2189de367064c605fa2ac95ed793124cb8af
elliotdelano <elliotdelano@gmail.com>
2021-09-28 14:28:10 -0400

    work

--- Bot.js
+++ Bot.js
@@ -1,18 +1,10 @@
-class Mine {
+class Food {
     constructor(position) {
         this.position = position
-
-        setTimeout(() => {
-            this.display()
-            projectiles.push(this)
-        }, 2000)
-        setTimeout(() => {
-            this.delete()
-        }, 30000)
     }
     display() {
         this.graphic = new PIXI.Graphics()
-        this.graphic.beginFill(0xff0000)
+        this.graphic.beginFill(0x00ff00)
         this.graphic.drawCircle(0, 0, 10)
         this.graphic.endFill()
         this.graphic.position.set(this.position.x, this.position.y)
@@ -32,172 +24,34 @@ class Mine {
 }
 
 class Bot {
-    position = new Vector2(0, 0)
+    position = new Vector2()
     velocity = new Vector2(Math.random(), Math.random())
-    acceleration = new Vector2(0, 0)
-    weight = 0
+    acceleration = new Vector2()
     maxSpeed = 0
     maxForce = 0.00
-    mineCooldown = 8000
+    viewrange = 0
+
+    food = 1000
+    static maxFood = 3000
 
     constructor(DNA, x, y) {
         this.position.set(x, y)
         this.DNA = DNA
-        this.structure = {}
-        this.unpack()
         this.display()
+        this.setup()
     }
-    // unpack() {
-    //     let genes = this.DNA.genes.split('&')
-    //     let genes2 = []
-    //     for (let gene of genes) {
-    //         genes2.push(gene.split('$'))
-    //     }
-    //     genes2.splice(0, 1)
-    //     for (let i = 0; i < genes2.length; i++) {
-    //         let pos = genes2[i][1].split(',')
-    //         this.structure[genes2[i][1]] = {
-    //             code: genes2[i][0],
-    //             x: parseInt(pos[0]),
-    //             y: parseInt(pos[1])
-    //         }
-    //         switch (genes2[i][0]) {
-    //             case "A":
-    //                 this.weight += 1
-    //                 break
-
-    //             case "B":
-    //                 this.weight += 1
-    //                 this.maxForce += 0.1
-    //                 break
-
-    //             case "C":
-    //                 this.weight += 1
-    //                 this.maxSpeed += 0.5
-    //                 break
-
-    //             case "D":
-    //                 this.weight += 1
-    //                 break
-
-    //             case "E":
-    //                 this.weight += 1
-    //                 this.mineCooldown -= 500
-    //                 break
-
-    //         }
-    //     }
-    //     let bot = this
-    //     this.mineInterval = setInterval(this.dropMine, this.mineCooldown, bot)
-    // }
-    unpack() {
-        this.used = []
-        let pos = new Vector2(0, 0)
-        let dir = new Vector2(1, 0)
-        for (let gene of this.DNA.genes) {
-            if (DNA.DirectionalGenes.includes(gene)) {
-                switch (gene) {
-                    case "I":
-                        dir.rotate2(90)
-                        break
-                    case "J":
-                        dir.rotate2(-90)
-                        break
-                }
-            } else {
-                this.structure[pos.x + ',' + pos.y] = {
-                    code: gene,
-                    x: pos.x,
-                    y: pos.y
-                }
-                this.used.push(pos.copy())
-                pos.add(this.findNextInLength(pos.copy(), dir.copy()))
-                switch (gene) {
-                    case "A":
-                        this.weight += 1
-                        break
-
-                    case "B":
-                        this.weight += 1
-                        this.maxForce += 0.1
-                        break
-
-                    case "C":
-                        this.weight += 1
-                        this.maxSpeed += 0.5
-                        break
-
-                    case "D":
-                        this.weight += 1
-                        break
-
-                    case "E":
-                        this.weight += 1
-                        this.mineCooldown -= 500
-                        break
-
-                }
-            }
-        }
-        let bot = this
-        this.mineInterval = setInterval(this.dropMine, this.mineCooldown, bot)
-    }
-    findNextInLength(pos, dir) {
-        let possible = pos.add(dir)
-        if (this.used.some(elm => elm.x === possible.x && elm.x === possible.y)) {
-            return this.findNextInLength(pos, dir.add(dir))
-        } else {
-            return dir
-        }
-    }
+    
     display() {
-        let max_x = -10000, max_y = -10000, min_x = 10000, min_y = 10000
         this.graphic = new PIXI.Graphics()
-        for (let key in this.structure) {
-            this.graphic.beginFill(Attachments[this.structure[key].code].color)
-            this.graphic.drawRect(this.structure[key].x * 10, this.structure[key].y * 10, 10, 10)
-            this.graphic.endFill()
-            max_x = this.structure[key].x * 10 > max_x ? this.structure[key].x * 10 : max_x
-            max_y = this.structure[key].y * 10 > max_y ? this.structure[key].y * 10 : max_y
-            min_x = this.structure[key].x * 10 < min_x ? this.structure[key].x * 10 : min_x
-            min_y = this.structure[key].y * 10 < min_y ? this.structure[key].y * 10 : min_y
-        }
-        max_y += 10
-        max_x += 10
-        this.width = max_x - min_x
-        this.height = max_y - min_y
-        //this.width = this.width > this.height ? this.width : this.height
-        //this.height = this.height > this.width ? this.height : this.width
-        this.bounds = new Polygon(this.position.copy(), [new Vector2(min_x, min_y), new Vector2(max_x, min_y), new Vector2(max_x, max_y), new Vector2(min_x, max_y)], this.width, this.height, true)
-        //this.bounds.translate(-this.width / 2, -this.height / 2)
-        //this.bounds.graphic.pivot.x -= this.width / 2
-        //this.bounds.graphic.pivot.y -= this.height / 2
-
-        this.view = new Polygon(this.position, [new Vector2(0, this.height), new Vector2(250, this.height), new Vector2(250, -this.height), new Vector2(0, -this.height)], null, null, false)
-        //this.view.rotate(Math.PI / 2)
+        this.graphic.beginFill(0xff0000)
+        this.graphic.drawPolygon(new PIXI.Point(0, 4), new PIXI.Point(8, 0), new PIXI.Point(0, -4))
         this.graphic.position.set(this.position.x - this.width / 2, this.position.y - this.height / 2)
         viewport.addChild(this.graphic)
     }
-    restructure() {
-        let max_x = -10000, max_y = -10000, min_x = 10000, min_y = 10000
-        this.graphic.clear()
-        for (let key in this.structure) {
-            this.graphic.beginFill(Attachments[this.structure[key].code].color)
-            this.graphic.drawRect(this.structure[key].x * 10, this.structure[key].y * 10, 10, 10)
-            this.graphic.endFill()
-            max_x = this.structure[key].x * 10 > max_x ? this.structure[key].x * 10 : max_x
-            max_y = this.structure[key].y * 10 > max_y ? this.structure[key].y * 10 : max_y
-            min_x = this.structure[key].x * 10 < min_x ? this.structure[key].x * 10 : min_x
-            min_y = this.structure[key].y * 10 < min_y ? this.structure[key].y * 10 : min_y
-        }
-        max_y += 10
-        max_x += 10
-        this.width = max_x - min_x
-        this.height = max_y - min_y
-        this.bounds.setPoints([new Vector2(min_x, min_y), new Vector2(max_x, min_y), new Vector2(max_x, max_y), new Vector2(min_x, max_y)])
-    }
-    dropMine(bot) {
-        new Mine(bot.position.copy())
+    setup() {
+        this.maxSpeed = this.DNA.genes[0]
+        this.maxForce = this.DNA.genes[1]
+        this.viewrange = this.DNA.genes[2]
     }
     wonder() {
         let dir = this.velocity.copy().normalize()
@@ -220,77 +74,41 @@ class Bot {
         return this.seek(target).mult(-1)
     }
     seek(target) {
-        let desired = new Vector2(target.x - this.position.x, target.y - this.position.y).setMag(this.maxSpeed).sub(this.velocity).limit(this.maxForce)
+        let desired = new Vector2(target.x - this.position.x, target.y - this.position.y)
+                        .setMag(this.maxSpeed)
+                        .sub(this.velocity)
+                        .limit(this.maxForce)
         //desired.setMag(this.maxSpeed)
         //desired.sub(this.velocity)
         //desired.limit(this.maxForce)
         return desired
     }
-    collideWith(other) {
-        if (other instanceof Mine) {
-            let dist = other.position.copy().sub(this.position)
-            let bestDist = Number.MAX_VALUE
-            let bestTile
-            for (let key in this.structure) {
-                let tile_vector = new Vector2(this.structure[key].x, this.structure[key].y)
-                tile_vector.setRotation(this.velocity.headingRads())
-                let d = dist.dist(tile_vector)
-                if (d < bestDist) {
-                    bestDist = d
-                    bestTile = key
-                }
-            }
-            if (!bestTile) {
-                other.delete()
-                return
-            }
-            switch (this.structure[bestTile].code) {
-                case "A":
-                    this.destroy()
-                    break
-
-                case "B":
-                    this.weight -= 1
-                    this.maxForce -= 0.1
-                    break
-
-                case "C":
-                    this.weight -= 1
-                    this.maxSpeed -= 0.5
-                    break
-
-                case "D":
-                    this.weight -= 1
-                    break
-
-                case "E":
-                    this.weight -= 1
-                    this.mineCooldown += 500
-                    break
-            }
-            delete this.structure[bestTile]
-            this.restructure()
-            other.delete()
-            return
-        }
-        if (this.justCollided) {
-            this.justCollided = false
-            return
-        }
-        this.position.sub(this.velocity.copy().mult(10))
-        this.velocity.mult(-1)
-        //this.acceleration.mult(0)
-        this.justCollided = true
-    }
     applyForce(force) {
-        this.acceleration.add(force.div(this.weight))
+        this.acceleration.add(force)
+    }
+    checkState() {
+        if(this.food > Bot.maxFood) {
+            this.reproduce()
+        }
+        if(this.food <= 0) {
+            this.destroy()
+        }
+    }
+    reproduce() {
+        let children = DNA.mitosis(this.DNA)
+        let c1 = new Bot(children[0], this.position.x, this.position.y)
+        let c2 = new Bot(children[0], this.position.x, this.position.y)
+        c1.velocity = this.velocity.copy()
+        c2.velocity = this.velocity.copy().mult(-1)
+        this.destroy()
+        Sim.population.push(c1)
+        Sim.population.push(c2)
     }
     destroy() {
         if (this.bounds.draw) {
             viewport.removeChild(this.bounds.graphic)
         }
         viewport.removeChild(this.graphic)
-        clearInterval(this.mineInterval)
         for (let i = 0; i < population.length; i++) {
             if (population[i] == this) {
                 population.splice(i, 1)
@@ -298,17 +116,19 @@ class Bot {
         }
     }
     update() {
-        if (this.position.x > world_size.width) {
-            this.position.x = world_size.width / 2
+        this.checkState()
+        this.food--
+        if (this.position.x > Sim.world_size.width) {
+            this.position.x = 0
         }
         if (this.position.x < 0) {
-            this.position.x = world_size.width / 2
+            this.position.x = Sim.world_size.width
         }
-        if (this.position.y > world_size.height) {
-            this.position.y = world_size.height / 2
+        if (this.position.y > Sim.world_size.height) {
+            this.position.y = 0
         }
         if (this.position.y < 0) {
-            this.position.y = world_size.height / 2
+            this.position.y = Sim.world_size.height
         }
         let rot = this.velocity.headingRads()
 
@@ -316,19 +136,19 @@ class Bot {
         this.velocity.limit(this.maxSpeed)
         this.position.add(this.velocity)
 
-        this.bounds.position.set(this.position.x, this.position.y)
-        this.bounds.rotate(rot)
-        if (this.bounds.graphic) {
-            this.bounds.graphic.position.set(this.position.x, this.position.y)
-            this.bounds.graphic.rotation = rot
-        }
+        // this.bounds.position.set(this.position.x, this.position.y)
+        // this.bounds.rotate(rot)
+        // if (this.bounds.graphic) {
+        //     this.bounds.graphic.position.set(this.position.x, this.position.y)
+        //     this.bounds.graphic.rotation = rot
+        // }
 
 
         this.graphic.position.set(this.position.x, this.position.y)
         this.graphic.rotation = rot
 
-        this.view.position.set(this.position.x, this.position.y)
-        this.view.rotate(rot)
+        //this.view.position.set(this.position.x, this.position.y)
+        //this.view.rotate(rot)
         //this.view.graphic.position.set(this.position.x, this.position.y)
         //this.view.graphic.rotation = rot
 
--- DNA.js
+++ DNA.js
@@ -1,118 +1,24 @@
 class DNA {
-    static DirectionalGenes = "IJ"
+    static mutationRate = 0.02;
     constructor(genesIn) {
-
-        this.adjacency = [{ x: 0, y: 1 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: -1, y: 0 }]
-        this.used = [{ x: 0, y: 0 }]
-        this.genes = genesIn || this.rand_struct()
-        console.log(this.genes)
+        this.genes = genesIn || [2.0, 0.1, 100]
     }
-    static crossover(a, b) {
-        let length = a.genes.length > b.genes.length ? a.genes.length : b.genes.length
-        let new_genome = ""
-        for (let i = 0; i < length; i++) {
-            if (i == 0) { new_genome += "A"; continue }
-            let geneA = a.genes.charAt(i)
-            let geneB = b.genes.charAt(i)
-            if (Math.random() > 0.97) {
-                new_genome += randomProperty(AttachmentPool).code
-            } else {
-                if (Math.random() > 0.5) {
-                    if (geneA) new_genome += geneA
-                } else {
-                    if (geneB) new_genome += geneB
-                }
+    static mitosis(old) {
+        let childA = DNA.mutate([].concat(old.genes))
+        let childB = DNA.mutate([].concat(old.genes))
+        
+        return { a: new DNA(childA), b: new DNA(childB) }
+    }
+    static mutate(genesIn) {
+        let genesOut = []
+        for(let gene of genesIn) {
+            if(Math.random() > 1-DNA.mutationRate) {
+                genesOut.push(gene + gene*0.1*rand_int(-1, 1))
             }
         }
-        let rnd = Math.random()
-        if (rnd > 0.75) {
-            new_genome += randomProperty(AttachmentPool).code
-        } else if (rnd > 0.5) {
-            new_genome = new_genome.slice(0, new_genome.length - 2)
-        }
-        return new DNA(new_genome)
+        return genesOut
     }
-    // rand_struct() {
-    //     let length = rand_int(3, 10)
-    //     let dna_strand = "&A$0,0"
-    //     for (let i = 0; i < length; i++) {
-    //         dna_strand = dna_strand + this.rand_struct_genome()
-    //     }
-    //     return dna_strand
-    // }
-    // rand_struct_genome() {
-    //     let p = this.rand_adj()
-    //     let pos = "$" + p.x + ',' + p.y
-    //     return "&" + randomProperty(AttachmentPool).code + pos
-    // }
-    rand_struct() {
-        let length = rand_int(3, 20)
-        let dna_strand = "A"
-        for (let i = 0; i < length; i++) {
-            dna_strand = dna_strand + this.rand_struct_genome()
-        }
-        return dna_strand
-    }
-    rand_struct_genome() {
-        return randomProperty(AttachmentPool).code
-    }
-    rand_adj() {
-        let pos = this.adjacency.splice(Math.floor(Math.random() * this.adjacency.length), 1)[0]
-        let neighbors = [{ x: pos.x, y: pos.y + 1 }, { x: pos.x + 1, y: pos.y }, { x: pos.x, y: pos.y - 1 }, { x: pos.x - 1, y: pos.y }]
-        this.used.push(pos)
-        for (let n of neighbors) {
-            if (!this.used.some(item => (item.x === n.x && item.y === n.y))) {
-                this.adjacency.push(n)
-            }
-        }
-        return pos
-    }
-    clone() {
-        return new DNA(this.genes)
+    getFitness() {
+        return sigmoid(this.genes[0]) + sigmoid(this.genes[1]) + sigmoid(this.genes[2])
     }
 }
-
-
-const Attachments = {
-    A: {
-        code: "A",
-        name: "core",
-        health: 20,
-        color: 0xffff00
-    },
-    B: {
-        code: "B",
-        name: "wheel",
-        health: 5,
-        color: 0xff0000
-    },
-    C: {
-        code: "C",
-        name: "engine",
-        health: 5,
-        color: 0xff00ff
-    },
-    D: {
-        code: "D",
-        name: "steel",
-        health: 30,
-        color: 0xffffff
-    },
-    E: {
-        code: "E",
-        name: "pewpew",
-        health: 5,
-        color: 0xD35000
-    },
-
-    //directional genes
-    I: {
-        code: "I"
-    },
-    J: {
-        code: "J"
-    }
-}
-
-let AttachmentPool = JSON.parse(JSON.stringify(Attachments))
-delete AttachmentPool.A
